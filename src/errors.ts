9831b8dffe185bc7cc1cace880f2b834ccb75baf
Jesus Gomez Bellido <jesus_jgb_45@hotmail.com>
2019-04-28 23:43:08 +0200

    feat: inject decorator created

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,4 @@
 export const ALREADY_REGISTER = (type: string) => `${type} is already registered`;
 export const NOT_REGISTER = (type: string) => `${type} is not register`;
 export const CIRCULAR_DEPENDENCIES = (type: string) => `${type} has circular dependencies`;
+export const NO_USE_DEPS = "You can't use property deps";
