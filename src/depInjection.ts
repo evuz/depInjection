dd0bbd332d9ed520bc20f54d1da041a22b97e0d2
Jesus Gomez Bellido <jesus_jgb_45@hotmail.com>
2019-04-28 22:33:28 +0200

    feat: error manage & control circular dependencies

--- src/depInjection.ts
+++ src/depInjection.ts
@@ -1,20 +1,23 @@
 import { IConstructor } from './types';
+import * as ERRORS from './errors';
 
 export class DepInjection {
-  private container = new Map<string, () => any>();
+  private container = new Map<string, (parents?: string[]) => any>();
 
   public get<T>(type: string): T {
-    const wrapper = this.container.get(type);
-    if (!wrapper) {
-      // TODO: Error
-      throw Error();
-    }
-    return wrapper();
+    return this.getInstance(type, []);
   }
 
   public register<T>(type: string, Injectable: IConstructor<T>): DepInjection {
-    this.container.set(type, () => {
-      const args = this.generateDeps(Injectable.deps || []);
+    if (this.container.has(type)) {
+      throw Error(ERRORS.ALREADY_REGISTER(type));
+    }
+
+    this.container.set(type, parents => {
+      if (parents.indexOf(type) > -1) {
+        throw Error(ERRORS.CIRCULAR_DEPENDENCIES(type));
+      }
+      const args = this.generateDeps(Injectable.deps || [], parents.concat(type));
       const instance = new Injectable(...args);
       this.container.set(type, function() {
         return instance;
@@ -24,7 +27,15 @@ export class DepInjection {
     return this;
   }
 
-  private generateDeps(deps: string[]) {
-    return deps.map(key => this.get(key));
+  private generateDeps(deps: string[], parents: string[]) {
+    return deps.map(key => this.getInstance(key, parents));
+  }
+
+  private getInstance<T>(type: string, parents: string[]): T {
+    const wrapper = this.container.get(type);
+    if (!wrapper) {
+      throw Error(ERRORS.NOT_REGISTER(type));
+    }
+    return wrapper(parents);
   }
 }
---
+++ src/errors.ts
@@ -0,0 +1,3 @@
+export const ALREADY_REGISTER = (type: string) => `${type} is already registered`;
+export const NOT_REGISTER = (type: string) => `${type} is not register`;
+export const CIRCULAR_DEPENDENCIES = (type: string) => `${type} has circular dependencies`;
