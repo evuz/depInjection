0c7223ccb1d225fcec3b2e99c9d7f79f4810c6bb
evuz <jesus_jgb_45@hotmail.com>
2019-08-02 23:02:12 +0200

    fix: decorator inject inverted the injections

--- src/__tests__/inject.spec.ts
+++ src/__tests__/inject.spec.ts
@@ -3,10 +3,11 @@ import { IConstructor } from '../types';
 
 const TYPES = {
   foo: 'Foo',
+  bar: 'Bar',
 };
 
 class Bar {
-  constructor(@inject(TYPES.foo) public foo) {}
+  constructor(@inject(TYPES.bar) public bar, @inject(TYPES.foo) public foo) {}
   public action() {
     this.foo.action();
   }
@@ -15,5 +16,10 @@ class Bar {
 describe('@inject', () => {
   test('should create a deps property', () => {
     expect((Bar as IConstructor<Bar>).deps).toContain(TYPES.foo);
+    expect((Bar as IConstructor<Bar>).deps).toContain(TYPES.bar);
+  });
+
+  test('should create a deps property in order', () => {
+    expect((Bar as IConstructor<Bar>).deps[0]).toBe(TYPES.bar);
   });
 });
