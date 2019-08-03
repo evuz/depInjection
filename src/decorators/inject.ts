33cb65c41f2e696ee3c50315e497dfd3ad454d1d
evuz <jesus_jgb_45@hotmail.com>
2019-08-03 09:41:13 +0200

    fix: remove log

--- src/decorators/inject.ts
+++ src/decorators/inject.ts
@@ -5,7 +5,6 @@ export function inject(type: string) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return function(target: IConstructor<any>, _propertyKey: string | symbol, _parameterIndex: number) {
     const deps = target.deps || [];
-    console.log(type);
     if (!Array.isArray(deps)) {
       throw Error(NO_USE_DEPS);
     }
