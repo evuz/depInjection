c0d0fbf96ee60e069a51ba4752cc0d2b30e0a08e
Jesús Gómez <jesgombel@outlook.es>
2021-09-17 09:00:12 +0200

    chore: update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,15 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import bundleSize from 'rollup-plugin-bundle-size';
-import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
+import typescript from 'rollup-plugin-typescript2'
+import bundleSize from 'rollup-plugin-bundle-size'
+import { terser } from 'rollup-plugin-terser'
 
-function commonPlugins() {
-  return [typescript(), terser(), bundleSize()];
-}
+import pkg from './package.json'
 
 export default [
-  // browser-friendly UMD build
-  {
-    input: 'src/depsin.ts',
-    output: {
-      name: 'depsin',
-      file: pkg.browser,
-      format: 'umd',
-    },
-    plugins: [resolve(), commonjs(), ...commonPlugins()],
-  },
-
   // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: 'src/depsin.ts',
-    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-    plugins: commonPlugins(),
-    output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
-  },
-];
+    external: [],
+    plugins: [typescript(), terser(), bundleSize()],
+    output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }]
+  }
+]
